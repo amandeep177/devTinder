39c7aeecf6633ca1f309619cd0cfd1aa3bb266b0
amandeep177 <bhardwajamandeep17@gmail.com>
2024-10-20 11:35:10 +0530

    writing auth middleware

--- src/app.js
+++ src/app.js
@@ -2,18 +2,32 @@ const express = require("express");
 const app = express();
 
 
-app.get("/user/:id/:password",(req,res)=>{
-    console.log(req.params);
+app.get("/user",
+    (req,res,next)=>{
     
-    res.send({firsname:"amandeep", college: "jims"})
-})
-app.post("/user",(req,res)=>{
-    res.send("data saved")
-})
+        next();
+    
+},
+ (req,res,next)=>{
+    console.log("in response 2");
+    
+    next()
+ },
+ (req,res,next)=>{
+     next();
+     
+ },
+ (req,res)=>{
+     res.send("Response 4")
+ },
+)
+// app.post("/user",(req,res)=>{
+//     res.send("data saved")
+// })
 
-app.get("/*fly$", (req,res)=>{
-    res.send("namaste hello regex")
-})
+// app.get("/*fly$", (req,res)=>{
+//     res.send("namaste hello regex")
+// })
 
 
 // app.use("/", (req,res)=>{
